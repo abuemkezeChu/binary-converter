3228bad52f3b81c7648628c6e991fa0cf9f389fe
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-20 21:55:19 +0100

    fix: lint errors

--- assets/scripts/converter.js
+++ assets/scripts/converter.js
@@ -9,13 +9,13 @@ const output = document.getElementById('output')
 
 function decimalToBinary(decimalNumber) {
   if (decimalNumber === 0) {
-    return "0";
+    return '0';
   }
 
-  let binary = "";
+  let binary = '';
   while (decimalNumber > 0) {
     binary = (decimalNumber % 2) + binary;
-    decimalNumber = Math.floor(decimalNumber / 2);
+    decimalNumber = Math.floor(decimalNumber / 2)
   }
 
   return binary;
@@ -30,15 +30,15 @@ function binaryToDecimal(binaryNumber) {
 // convert from string to decimal
 
 function stringToBinary(string) {
-  let binary = "";
+  let binary = '';
   for (let i = 0; i < string.length; i++) {
-    const charCode = string.charCodeAt(i);
+    const charCode = string.charCodeAt(i)
 
     // converts each character's Unicode code point to an 8-bit binary representation and ensures that it is padded with zeros on the left if necessary
 
-    binary += charCode.toString(2).padStart(8, "0");
+    binary += charCode.toString(2).padStart(8, '0')
   }
-  return binary;
+  return binary
 }
 
 // check for the conversion type
